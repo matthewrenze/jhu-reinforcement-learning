db013e1babc420a5f84d2b9ebc643acadf8058c2
Matthew Renze <matthew@matthewrenze.com>
2024-03-19 11:03:03 -0700

    Added clyde chase mode

--- source/ghosts/clyde_ghost.py
+++ source/ghosts/clyde_ghost.py
@@ -8,3 +8,10 @@ class ClydeGhost(Ghost):
         tile = Tile.CLYDE
         scatter_target = (20, 0)
         super().__init__(tile, location, scatter_target, house)
+
+    def get_chase_target(self, agent_location: tuple[int, int], agent_orientation: int, ghost_locations: list[tuple[int, tuple[int, int]]]) -> tuple[int, int]:
+        distance = self.calculate_distance(agent_location, self.location)
+        if distance > 8:
+            return agent_location
+        else:
+            return self.scatter_target
--- source/ghosts_tests/clyde_ghost_tests.py
+++ source/ghosts_tests/clyde_ghost_tests.py
@@ -1,1 +1,30 @@
-# TODO: To be implemented
+import pytest
+from ghosts.clyde_ghost import ClydeGhost
+from houses.house import House
+from tiles.tile import Tile
+
+@pytest.fixture
+def setup():
+    clyde = ClydeGhost((0, 0), House([(0, 0)], (1, 1)))
+    return clyde
+
+def test_init(setup):
+    clyde = setup
+    assert clyde.tile == Tile.CLYDE
+    assert clyde.scatter_target == (20, 0)
+
+@pytest.mark.parametrize("agent_location, expected_target", [
+    ((0, 8), (20, 0)),
+    ((0, 9), (0, 0)),
+    ((8, 0), (20, 0)),
+    ((9, 0), (0, 0)),
+    ((7, 7), (20, 0)),
+    ((9, 9), (0, 0))])
+def test_get_chase_target(setup, agent_location, expected_target):
+    clyde = setup
+    agent_location = (0, 8)
+    agent_orientation = 1
+    ghost_locations = [(Tile.CLYDE.id, (4, 0))]
+    expected_target = (20, 0)
+    actual_target = clyde.get_chase_target(agent_location, agent_orientation, ghost_locations)
+    assert actual_target == expected_target
