70fcddda7e4b15ee0b930555e44db725e51d849f
Simran Shinh <sshinh1225@gmail.com>
2024-04-03 18:21:17 -0400

    working features and unit tests (except binary flag)

--- source/agents/feature_extraction.py
+++ source/agents/feature_extraction.py
@@ -3,85 +3,121 @@ import numpy as np
 from states.state import State
 from tiles.tile import Tile
 from tiles.tiles import Tiles
-from actions.action import Action
+from ghosts.ghost import Ghost, Mode
 
 class FeatureExtraction():
-    def __init__(self, tiles:Tiles, state:State):
+    def __init__(self, tiles:Tiles, state:State, ghosts:list[Ghost]):
         self._tiles = tiles
         self._current_state = state
+        self._ghosts = ghosts
 
     def distance_closest_food(self): 
-        distance = self._find_minimum_distance(self._current_state, Tile.DOT)
+        distance = self._find_minimum_distance(self._current_state, [Tile.DOT])
         return distance
 
     def distance_closest_ghost(self): 
-        ghost_tiles = [Tile.BLINKY, Tile.INKY, Tile.PINKY, Tile.CLYDE]
+        ghost_tiles = [Tile.BLINKY, Tile.INKY, Tile.PINKY, Tile.CLYDE, Tile.STATIC]
         distance = self._find_minimum_distance(self._current_state, ghost_tiles)
         return distance 
     
     def number_active_ghosts_1step(self): 
-        pass
+        num_ghosts = self._ghosts_1step_away(self._current_state, self._ghosts, Mode.CHASE)
+        return num_ghosts
 
     def number_active_ghosts_2step(self): 
-        pass
+        num_ghosts = self._ghosts_2steps_away(self._current_state, self._ghosts, Mode.CHASE)
+        return num_ghosts
 
     def number_scared_ghosts_1step(self): 
-        pass
+        num_ghosts = self._ghosts_1step_away(self._current_state, self._ghosts, Mode.FRIGHTENED)
+        return num_ghosts
 
     def number_scared_ghosts_2step(self): 
-        pass
+        num_ghosts = self._ghosts_2steps_away(self._current_state, self._ghosts, Mode.FRIGHTENED)
+        return num_ghosts
 
     def safety_mode(self): 
         pass
 
-    def _find_minimum_distance(self, state:State, desired_tile:Tile): 
+    def _find_minimum_distance(self, state:State, desired_tiles:list[Tile]): 
         current_position = state.agent_location
-        search_locations = [(current_position[0], current_position[1])]
         distance = 0
+        search_locations = [(current_position[0], current_position[1], distance)]
+        # Make sure only unique search locations are checked 
+        search_tracker = set()
+        
         while len(search_locations) != 0: 
-            position_x, position_y = search_locations.pop(0)
-            if self._tiles[(position_x, position_y)] == desired_tile: 
-                return distance
+            position_x, position_y, distance = search_locations.pop(0)
+            if (position_x, position_y) in search_tracker: 
+                continue
+            search_tracker.add((position_x, position_y))
+            for i in desired_tiles:
+                if self._tiles[(position_x, position_y)] == i: 
+                    return distance
             else: 
                 distance += 1
-                legal_positions = self._find_legal_positions(current_position)
+                legal_positions = self._find_legal_positions((position_x, position_y))
+                # All legal positions are the same distance away
                 for i in legal_positions: 
-                    search_locations.append(i)
+                    search_locations.append((i[0], i[1], distance))
         return None
 
 
     def _find_legal_positions(self, current_position):
-
         legal_positions = []
-        transitions = [(-1,0),(0,1),(1,0),(0,-1)]
-        for i in transitions: 
-            print(i)
-     
-            new_row = current_position[0] + i[0]
-            new_col = current_position[1] + i[1]
-            print((new_row, new_col))
-
-            if not self._tiles[(new_row, new_col)] == Tile.WALL :
-                print("LOOP")
-                legal_positions.append((new_row, new_col))
-
-
+        possible_positions = self._1step_positions((current_position[0], current_position[1]))
+        for i in possible_positions:
+            # Check new row and col are valid and not a wall tile
+            if (i[0] >= 0) and (i[1] >= 0) and not self._tiles[(i[0], i[1])] == Tile.WALL :
+                legal_positions.append((i[0], i[1]))
         return legal_positions
 
-    def _tiles_1step_away(self, current_position, desired_tile:Tile): 
-        desired_1step = False
 
+    def _ghosts_1step_away(self, state:State, ghosts: list[Ghost], mode:Mode): 
+        current_position = state.agent_location
+        ghost_locations = []
+        num_desired = 0
+
+        for ghost in ghosts: 
+            if ghost.mode == mode: 
+                ghost_locations.append(ghost.location)
+          
+        new_locations = self._1step_positions((current_position[0], current_position[1]))
+        for i in new_locations:
+            if (i[0] >= 0) and (i[1] >= 0) and (i[0], i[1]) in ghost_locations:
+                num_desired += 1
+            
+        return num_desired
+    
+
+    def _ghosts_2steps_away(self, state:State, ghosts: list[Ghost], mode:Mode): 
+        current_position = state.agent_location
+        ghost_locations = []
+        found_ghosts = set()
+
+        for ghost in ghosts: 
+            if ghost.mode == mode: 
+                ghost_locations.append(ghost.location)
+        
+        step_locations = self._1step_positions(current_position)
+        for i in step_locations: 
+            step2_locations = self._1step_positions((i[0],i[1]))
+            for j in step2_locations: 
+                if (j[0] >= 0) and (j[1] >= 0) and (j[0], j[1]) in ghost_locations:
+                    found_ghosts.add((j[0],j[1]))
+        
+        return len(found_ghosts)
+            
+
+    def _1step_positions(self, starting_position):
+        new_locations = []
         transitions = [(-1,0),(0,1),(1,0),(0,-1)]
         for i in transitions: 
-            new_row = current_position[0] + i[0]
-            new_col = current_position[1] + i[1]
-            if ~self._tiles[(new_row, new_col)] in desired_tile:
-                desired_1step = True
+            new_locations.append((starting_position[0] + i[0],starting_position[1] + i[1]))
+        
+        return new_locations
+                                 
             
-        return desired_1step
-    
 
-    def _tiles_2steps_away(current_position, tiles): 
-        pass
-    
+        
 
--- source/tests/agents_tests/feature_extraction_tests.py
+++ source/tests/agents_tests/feature_extraction_tests.py
@@ -5,54 +5,63 @@ from agents.feature_extraction import FeatureExtraction
 from tiles.test_tiles import TestTiles
 from tiles.tile import Tile
 from states.state import State
+from ghosts.ghost import Mode
 
 @pytest.fixture()
 def setup():
 
     '''   #  #  #  #  #
-          #  .  s  .  #
-          #  .  #  o  #
-          #  .  c  .  #
+          #  .  .  .  #
+          #  .  #  s  #
+          #  p  c     #
           #  #  #  #  #
     '''
 
-    tiles = TestTiles.create([[1, 1, 1, 1, 1], [1, 3, 5, 3, 1], [1, 3, 1, 4, 1], [1, 3, 2, 3, 1], [1, 1, 1, 1, 1]])
+    tiles = TestTiles.create([[1, 1, 1, 1, 1], [1, 3, 3, 3, 1], [1, 3, 1, 5, 1], [1, 7, 2, 0, 1], [1, 1, 1, 1, 1]])
     state = Mock()
     state.agent_location = (3,2)
-    feature_extraction = FeatureExtraction(tiles, state)
+
+    pinky_ghost = Mock()
+    static_ghost = Mock()
+    pinky_ghost.mode = Mode.FRIGHTENED
+    pinky_ghost.location = (3, 1)
+    static_ghost.mode = Mode.CHASE
+    static_ghost.location = (2,3)
+    ghosts = [static_ghost, pinky_ghost]
+    feature_extraction = FeatureExtraction(tiles, state, ghosts)
     return tiles, feature_extraction
 
 def test_distance_closest_food(setup): 
     _, feature_extraction = setup
     food_distance = feature_extraction.distance_closest_food()
-    assert food_distance == 1
+    assert food_distance == 2
 
 def test_distance_closest_ghost(setup):
     _, feature_extraction = setup
     ghost_distance = feature_extraction.distance_closest_ghost()
-    assert ghost_distance == 4
+    assert ghost_distance == 1
 
 def test_number_active_ghosts_1step(setup): 
-    tiles, feature_extraction = setup
-    num_ghosts = feature_extraction.number_active_ghosts_1step(tiles)
+    _, feature_extraction = setup
+    num_ghosts = feature_extraction.number_active_ghosts_1step()
     assert num_ghosts == 0 
-
-def test_number_active_ghosts_2step(setup): 
-    tiles, feature_extraction = setup
-    num_ghosts = feature_extraction.number_active_ghosts_2step(tiles)
-    assert num_ghosts == 0
 
 def test_number_scared_ghosts_1step(setup): 
+    _, feature_extraction = setup
+    num_ghosts = feature_extraction.number_scared_ghosts_1step()
+    assert num_ghosts == 1
+    
+def test_number_active_ghosts_2step(setup): 
     tiles, feature_extraction = setup
-    num_ghosts = feature_extraction.number_scared_ghosts_1step(tiles)
-    assert num_ghosts == 0 
+    num_ghosts = feature_extraction.number_active_ghosts_2step()
+    assert num_ghosts == 1
 
 def test_number_scared_ghosts_2step(setup): 
     tiles, feature_extraction = setup
-    num_ghosts = feature_extraction.number_scared_ghosts_2step(tiles)
+    num_ghosts = feature_extraction.number_scared_ghosts_2step()
     assert num_ghosts == 0
 
-def test_safety_mode(setup): 
-    tiles, feature_extraction = setup
-    safety = feature_extraction.safety_mode(tiles)
-    assert safety == True 
+# def test_safety_mode(setup): 
+#     tiles, feature_extraction = setup
+#     safety = feature_extraction.safety_mode(tiles)
+#     assert safety == True 
